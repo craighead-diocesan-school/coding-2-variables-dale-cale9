c77bcf9a0a4e270025a1515ec00ce852fbc44199
dale-cale9 <145309461+dale-cale9@users.noreply.github.com>
2024-06-18 10:23:19 +1200

    gst + dog age

--- code/1-pupper-age.js
+++ code/1-pupper-age.js
@@ -6,4 +6,7 @@ function pupperAge() {
   alert('Dog years are human years times seven.')
 
   let humanYears = prompt('How old is your dog in human years?')
+  let age = humanYears * 7
+
+  alert('Your dog is ' + age + ' Years old!')
 }
--- code/2-gst-calculator.js
+++ code/2-gst-calculator.js
@@ -4,4 +4,8 @@
 
 function gstCalculator() {
   alert('The price with GST the price and 15%.')
+let price = prompt( 'Whats the price of the item?')
+let result = price * 1.5
+alert( 'The price of the idem after GST added is' + result)
+
 }
