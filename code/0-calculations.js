0a7c5bbf474ea2cb6f38e1b36ffef2e02a35db6d
dale-cale9 <145309461+dale-cale9@users.noreply.github.com>
2024-06-18 10:05:04 +1200

    .

--- code/0-calculations.js
+++ code/0-calculations.js
@@ -2,4 +2,19 @@
 // #### ----- Calculations ----- ######
 // ####################################
 
-function calculations() {}
+function calculations() {
+//alert (8+2)
+//alert (8-2)
+//alert (8*2)
+//alert (10/4)
+
+// let firstNumber = 20
+// let secoundNumber  = 5
+// let result = firstNumber * secoundNumber
+// alert(result)
+
+firstNumber = prompt ('Enter a number')
+secoundNumber = 2 
+result = firstNumber * secoundNumber
+alert(firstNumber + ' x ' + secoundNumber + " = " + result)
+}
