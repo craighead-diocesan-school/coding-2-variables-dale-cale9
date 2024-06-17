fd10e6f08e19ab65edc6e97446384c1f471299e8
dale-cale9 <145309461+dale-cale9@users.noreply.github.com>
2024-06-18 10:30:46 +1200

    secounds left + years left

--- code/3-lessons-to-seconds.js
+++ code/3-lessons-to-seconds.js
@@ -4,4 +4,8 @@
 
 function lessonsToSeconds() {
   alert('There are 60 minutes in a lesson, and 60 seconds in a minute.')
+
+  let lessons = prompt('How mnay lesson do you have left?')
+  let result = lessons * 60 * 60
+  alert('you have ' + result+ ' secounds left of school 😭')
 }
--- code/4-test-life-remaining.js
+++ code/4-test-life-remaining.js
@@ -3,5 +3,8 @@
 // ####################################
 
 function lifeRemaining() {
+    let age = prompt( ' How old are you?')
+    let result =  83.5 - age
+    alert( 'You have ' + result + ' years left to live💀')
   
 }
